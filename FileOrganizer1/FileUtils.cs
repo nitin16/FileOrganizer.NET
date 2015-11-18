4e297191dba939418075c6e0a4ee12db2b721be0
nitin bafna <nitinb@terrace.com>
2015-11-18 07:59:37 -0800

    async and sync methods for file organizer

---
+++ FileOrganizer1/FileUtils.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FileOrganizer
+{
+    public class FileUtils
+    {
+        string sourceDir
+        {
+            get;
+            set;
+        }
+
+        string destDir
+        {
+            get;
+            set;
+        }
+        
+        //we init this once so that if the function is repeatedly called
+        //it isn't stressing the garbage man
+        private Regex r = new Regex(":");
+
+        public FileUtils(string sourceDir, string destDir)
+        {
+            this.sourceDir = sourceDir;
+            this.destDir = destDir;
+        }
+
+        public IEnumerable<Task> ProcessDir(string dirpath)
+        {
+            Console.WriteLine("Processecing Dir: " + dirpath);
+
+            var fileNames= from filename in Directory.EnumerateFiles(dirpath, "*.*", SearchOption.AllDirectories)
+                            select filename;
+
+            return fileNames.AsParallel().Select(file => ProcessFile(file));//.SelectMany(t=> t);
+            //var tasks =  subDirectories.AsParallel().Select(subDirPath => ProcessDir(subDirPath)).SelectMany(t=> t);
+            
+            //IEnumerable<Task> processFileTasks = dir.GetFiles().ToList().Select(file => ProcessFile(file));
+            //await Task.WhenAll(processDirTasks);
+            //await Task.WhenAll(processFileTasks);
+
+            //foreach (var subDir in dir.GetDirectories())
+            //{
+            //    Console.WriteLine("Processecing Dir: " + subDir.FullName);
+            //    ProcessDir(subDir);
+            //}
+
+            //foreach (var file in dir.GetFiles())
+            //{
+            //    ProcessFile(file);
+            //}
+        }
+
+        private async Task ProcessFile(string filePath)
+        {
+            var file = new FileInfo(filePath);
+            if (file == null)
+            {
+                return;
+            }
+            //Console.WriteLine("Processecing file : " + file.FullName);
+
+            var targetDir = destDir + "/" + GetTargetDir(file.FullName).Result;
+            Directory.CreateDirectory(targetDir);
+
+            file.CopyTo(targetDir + file.Name, true);
+            //Console.WriteLine("Copied sucessesfully: " + targetDir + file.Name);
+        }
+
+
+        //retrieves the datetime WITHOUT loading the whole image
+        public Task<DateTime> GetDateTakenFromImage(string path)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            using (Image myImage = Image.FromStream(fs, false, false))
+            {
+                PropertyItem propItem = myImage.GetPropertyItem(36867);
+                string dateTaken = r.Replace(Encoding.UTF8.GetString(propItem.Value), "-", 2);
+                return Task.FromResult(DateTime.Parse(dateTaken));
+            }
+        }
+
+        public async Task<string> GetTargetDir(string FilePath)
+        {
+            try
+            {
+                var date = await GetDateTakenFromImage(FilePath);
+                return date.ToString("yyyy/MM/");
+            }
+            catch
+            {
+                return "/Exceptions/";
+            }
+        }
+
+        public void processDirSync(string dirpath)
+        {
+            var dir = new DirectoryInfo(dirpath);
+            foreach (var subDir in dir.GetDirectories())
+            {
+                Console.WriteLine("Processecing Dir: " + subDir.FullName);
+                processDirSync(subDir.FullName);
+            }
+
+            foreach (var file in dir.GetFiles())
+            {
+                ProcessFile(file.FullName);
+            }
+        }
+    }
+}
--- FileOrganizer1/Program.cs
+++ FileOrganizer1/Program.cs
@@ -1,91 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.IO;
-using System.Text.RegularExpressions;
-using System.Text;
-using System.Drawing;
-using System.Drawing.Imaging;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace FileOrganizer
 {
     public class Program
     {
-        static string sourceDir;
-        static string destDir;
+        
         public static void Main(string[] args)
         {
             if (string.IsNullOrWhiteSpace(args[0]) || string.IsNullOrWhiteSpace(args[1]))
             {
                 Console.WriteLine("Missing Source or destination directories");
             }
-             sourceDir = args[0];
-             destDir = args[1];
 
-            Console.WriteLine("Processecing Dir: " + sourceDir);
-            var dir = new DirectoryInfo(sourceDir);
-            ProcessDir(dir);
+            var sourceDir = args[0];
+            var destDir = args[1];
+            FileUtils helper = new FileUtils(sourceDir, destDir);
+
+            var startTime = DateTime.Now;
+            Task.WaitAll(helper.ProcessDir(sourceDir).ToArray());
+            //helper.processDirSync(sourceDir);
+            Console.WriteLine("Processing Complete");
+            Console.WriteLine(String.Format("Total time: {0}", DateTime.Now - startTime));
+            
             Console.ReadLine();
         }
 
-        private static void ProcessDir(DirectoryInfo dir)
-        {
-            foreach (var subDir in dir.GetDirectories())
-            {
-                Console.WriteLine("Processecing Dir: " + subDir.FullName);
-                ProcessDir(subDir);
-            }
-
-            foreach (var file in dir.GetFiles())
-            {
-                ProcessFile(file);
-            }
-        }
-
-        private static void ProcessFile(FileInfo file)
-        {
-            return;
-
-            if (file == null)
-            {
-                return;
-            }
-            Console.WriteLine("Processecing file : " + file.FullName);
-
-            var targetDir = destDir + "/" + GetTargetDir(file.FullName);
-            Directory.CreateDirectory(targetDir);
-            
-            file.CopyTo(targetDir + file.Name, true);
-            Console.WriteLine("Copied sucessesfully: " + targetDir + file.Name);
-        }
-
-        //we init this once so that if the function is repeatedly called
-        //it isn't stressing the garbage man
-        private static Regex r = new Regex(":");
-
-        //retrieves the datetime WITHOUT loading the whole image
-        public static DateTime GetDateTakenFromImage(string path)
-        {
-            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
-            using (Image myImage = Image.FromStream(fs, false, false))
-            {
-                PropertyItem propItem = myImage.GetPropertyItem(36867);
-                string dateTaken = r.Replace(Encoding.UTF8.GetString(propItem.Value), "-", 2);
-                return DateTime.Parse(dateTaken);
-            }
-        }
-
-        public static string GetTargetDir(string FilePath)
-        {
-            try
-            {
-                return GetDateTakenFromImage(FilePath).ToString("yyyy/MM/");
-            }
-            catch
-            {
-                return "/Exceptions/";
-            }
-        }
     }
 }
